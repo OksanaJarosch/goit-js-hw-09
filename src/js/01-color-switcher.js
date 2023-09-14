9d980cb73a23363abaecb2e5697f8245bfe72bf7
Oksana Jarosch <ok.jarosch@gmail.com>
2023-09-15 00:53:55 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
 
+const startEl = document.querySelector('[data-start]');
+const stopEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+console.log(bodyEl);
+
+startEl.addEventListener('click', onStart);
+
+let onInterval = false;
+stopEl.disabled = true;
+
+function onStart() {
+    const IntId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+        onInterval = true;
+        console.log('I am ON');
+
+        if (onInterval) {
+            startEl.disabled = true;
+            stopEl.disabled = false;
+        } 
+
+        stopEl.addEventListener('click', onStop);
+        function onStop() {
+            clearInterval(IntId);
+            onInterval = false;
+            startEl.disabled = false;
+            stopEl.disabled = true;
+        }
+    }, 1000);
+}
+
+// random color function
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
