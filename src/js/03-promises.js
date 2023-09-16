45ca299b30e778cee3dbf62ba06e09afeebf48e8
Oksana Jarosch <ok.jarosch@gmail.com>
2023-09-16 23:43:32 +0200

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
+import throttle  from "lodash.throttle";
+
+const userDate = {};
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onSubmit);
+
+//-------function for submit button----------
+function onSubmit(event) {
+event.preventDefault();
+
+for (let i = 0; i < userDate.amount; i += 1){
+const stepDelay = userDate.delay + userDate.step * i;
+
+createPromise(i + 1, stepDelay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+}
+}
+//-------------------------------------------
+
+
+function createPromise(positionDate, delayDate) {
+  return new Promise((resolve, reject) => {
+    setTimeout((delay) => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+  resolve({position: positionDate, delay: delayDate})
   } else {
-    // Reject
+    reject({position: positionDate, delay: delayDate})
   }
+}, delayDate)
+})
+}
+
+function onInput(event) {
+if (event.target.name === 'delay') {
+    userDate.delay = +(event.target.value);
+}
+if (event.target.name === 'step') {
+  userDate.step = +(event.target.value);
+}
+if (event.target.name === 'amount') {
+  userDate.amount = +(event.target.value);
+}
 }
